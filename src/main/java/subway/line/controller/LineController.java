bbe13f2220d5e41bcc3be253ebbe2c564d58582a
santos019 <santos019@naver.com>
2024-07-13 15:35:10 +0900

    feat: add showLines method in LineController class

--- src/main/java/subway/line/controller/LineController.java
+++ src/main/java/subway/line/controller/LineController.java
@@ -1,14 +1,18 @@
 package subway.line.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import subway.line.dto.CreateLineRequest;
 import subway.line.dto.CreateLineResponse;
+import subway.line.dto.RetrieveLineResponse;
 import subway.line.service.LineService;
+import subway.station.dto.StationResponse;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class LineController {
@@ -22,6 +26,11 @@ public class LineController {
     public ResponseEntity<CreateLineResponse> createLine(@RequestBody CreateLineRequest createLineRequest) throws Exception {
         CreateLineResponse line = lineService.saveStation(createLineRequest);
         return ResponseEntity.created(URI.create("/line/" + line.getId())).body(line);
+    }
+
+    @GetMapping(value = "/lines")
+    public ResponseEntity<RetrieveLineResponse> showStations() {
+        return ResponseEntity.ok().body(lineService.findAllLines());
     }
 
 }
