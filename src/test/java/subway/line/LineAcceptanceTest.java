d1868ffe437770c387aa7af773733e840ac6d41c
santos019 <santos019@naver.com>
2024-07-17 00:23:08 +0900

    test: refactor LineAcceptanceTest

--- src/test/java/subway/line/LineAcceptanceTest.java
+++ src/test/java/subway/line/LineAcceptanceTest.java
@@ -1,48 +1,38 @@
 package subway.line;
 
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
-import subway.line.dto.CreateLineRequest;
 import subway.line.dto.CreateLineResponse;
 import subway.line.dto.ModifyLineRequest;
 import subway.line.dto.RetrieveLineResponse;
-import subway.station.dto.CreateStationResponse;
+import subway.station.dto.StationResponse;
 
-import java.util.*;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static subway.util.LineStep.*;
+import static subway.util.StationStep.여러_개의_지하철_역_생성;
 
 @DisplayName("지하철 노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
+
 
     @DisplayName("지하철 노선을 생성한다")
     @Test
     @Sql(scripts = "classpath:truncate-tables.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     public void createLine() {
         // given
-        List<CreateStationResponse> 지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역"));
+        List<StationResponse> 지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역"));
 
         // when
+        CreateLineResponse 원본_지하철_노선 = new CreateLineResponse("신분당선", "Red", List.of(지하철_역_목록.get(0), 지하철_역_목록.get(1)), 10L);
         CreateLineResponse 생성된_지하철_노선 = 지하철_노선_생성("신분당선", "Red", 지하철_역_목록.get(0).getId(), 지하철_역_목록.get(1).getId(), 10L);
 
         // then
-        assertThat(생성된_지하철_노선.getColor()).isEqualTo("Red");
-        assertThat(생성된_지하철_노선.getDistance()).isEqualTo(10L);
-        assertThat(생성된_지하철_노선.getStations().size()).isEqualTo(2);
-
-        for (int i = 0; i < 생성된_지하철_노선.getStations().size(); i++) {
-            assertThat(생성된_지하철_노선.getStations().get(i).getId()).isEqualTo(지하철_역_목록.get(i).getId());
-            assertThat(생성된_지하철_노선.getStations().get(i).getName()).isEqualTo(지하철_역_목록.get(i).getName());
-        }
-
+        assertThat(생성된_지하철_노선.equals(원본_지하철_노선)).isTrue();
     }
 
     @DisplayName("전체 지하철 노선을 조회한다")
@@ -50,14 +40,13 @@ public class LineAcceptanceTest {
     @Sql(scripts = "classpath:truncate-tables.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     public void retrieveAllLine() {
         // given
-        List<CreateStationResponse> 생성된_지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역", "선릉역", "논현역"));
+        List<StationResponse> 생성된_지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역", "선릉역", "논현역"));
 
         RetrieveLineResponse 비교할_지하철_노선_목록 = new RetrieveLineResponse(List.of((지하철_노선_생성("신분당선", "Red", 생성된_지하철_역_목록.get(0).getId(), 생성된_지하철_역_목록.get(1).getId(), 10L)),
                 지하철_노선_생성("분당선", "Red", 생성된_지하철_역_목록.get(2).getId(), 생성된_지하철_역_목록.get(3).getId(), 20L)));
 
         // when
         RetrieveLineResponse 지하철_노선_목록 = 지하철_전체_노선_조회();
-        지하철_노선_목록.getCreateLineResponseList().sort(Comparator.comparingLong(CreateLineResponse::getId));
 
         // then
         assertThat(지하철_노선_목록.getCreateLineResponseList().size()).isEqualTo(2);
@@ -65,17 +54,7 @@ public class LineAcceptanceTest {
             CreateLineResponse 비교할_지하철_노선 = 비교할_지하철_노선_목록.getCreateLineResponseList().get(i);
             CreateLineResponse 지하철_노선 = 지하철_노선_목록.getCreateLineResponseList().get(i);
 
-            assertThat(비교할_지하철_노선.getId()).isEqualTo(지하철_노선.getId());
-            assertThat(비교할_지하철_노선.getName()).isEqualTo(지하철_노선.getName());
-            assertThat(비교할_지하철_노선.getColor()).isEqualTo(지하철_노선.getColor());
-            assertThat(비교할_지하철_노선.getDistance()).isEqualTo(지하철_노선.getDistance());
-
-            for (int j = 0; j < 비교할_지하철_노선.getStations().size(); j++) {
-                CreateStationResponse 비교할_지하철_역 = 비교할_지하철_노선.getStations().get(j);
-                CreateStationResponse 지하철_역 = 지하철_노선.getStations().get(j);
-                assertThat(비교할_지하철_역.getId()).isEqualTo(지하철_역.getId());
-                assertThat(비교할_지하철_역.getName()).isEqualTo(지하철_역.getName());
-            }
+            assertThat(비교할_지하철_노선.equals(지하철_노선)).isTrue();
         }
 
     }
@@ -85,7 +64,7 @@ public class LineAcceptanceTest {
     @Sql(scripts = "classpath:truncate-tables.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     public void retrieveLine() {
         // when
-        List<CreateStationResponse> 생성된_지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역", "선릉역", "논현역"));
+        List<StationResponse> 생성된_지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역", "선릉역", "논현역"));
         RetrieveLineResponse 비교할_지하철_노선_목록 = new RetrieveLineResponse();
         CreateLineResponse 지하철_노선_1 = 지하철_노선_생성("신분당선", "Red", 생성된_지하철_역_목록.get(0).getId(), 생성된_지하철_역_목록.get(1).getId(), 10L);
         비교할_지하철_노선_목록.getCreateLineResponseList().add(지하철_노선_1);
@@ -95,18 +74,7 @@ public class LineAcceptanceTest {
 
         // then
         CreateLineResponse 비교할_지하철_노선 = 비교할_지하철_노선_목록.getCreateLineResponseList().get(0);
-
-        assertThat(비교할_지하철_노선.getId()).isEqualTo(지하철_노선.getId());
-        assertThat(비교할_지하철_노선.getName()).isEqualTo(지하철_노선.getName());
-        assertThat(비교할_지하철_노선.getColor()).isEqualTo(지하철_노선.getColor());
-        assertThat(비교할_지하철_노선.getDistance()).isEqualTo(지하철_노선.getDistance());
-
-        for (int j = 0; j < 비교할_지하철_노선.getStations().size(); j++) {
-            CreateStationResponse 비교할_지하철_역 = 비교할_지하철_노선.getStations().get(j);
-            CreateStationResponse 지하철_역 = 지하철_노선.getStations().get(j);
-            assertThat(비교할_지하철_역.getId()).isEqualTo(지하철_역.getId());
-            assertThat(비교할_지하철_역.getName()).isEqualTo(지하철_역.getName());
-        }
+        assertThat(비교할_지하철_노선.equals(지하철_노선)).isTrue();
 
     }
 
@@ -115,7 +83,7 @@ public class LineAcceptanceTest {
     @Sql(scripts = "classpath:truncate-tables.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     public void modifyStation() {
         // given
-        List<CreateStationResponse> 생성된_지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역", "선릉역", "논현역"));
+        List<StationResponse> 생성된_지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역", "선릉역", "논현역"));
         CreateLineResponse 지하철_노선_1 = 지하철_노선_생성("신분당선", "Red", 생성된_지하철_역_목록.get(0).getId(), 생성된_지하철_역_목록.get(1).getId(), 10L);
 
         ModifyLineRequest 지하철_노선_변경_요청 = new ModifyLineRequest("바꾼_신분당선", "Blue");
@@ -135,101 +103,11 @@ public class LineAcceptanceTest {
     @Sql(scripts = "classpath:truncate-tables.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     public void deleteStation() {
         // given
-        List<CreateStationResponse> 생성된_지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역", "선릉역", "논현역"));
+        List<StationResponse> 생성된_지하철_역_목록 = 여러_개의_지하철_역_생성(List.of("강남역", "역삼역", "선릉역", "논현역"));
         CreateLineResponse 지하철_노선_1 = 지하철_노선_생성("신분당선", "Red", 생성된_지하철_역_목록.get(0).getId(), 생성된_지하철_역_목록.get(1).getId(), 10L);
 
         // when & then
         지하철_노선_삭제(지하철_노선_1.getId());
     }
 
-    public void 지하철_노선_삭제(Long deleteStationIdx) {
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .body(deleteStationIdx)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete("/lines/" + deleteStationIdx)
-                .then().log().all()
-                .extract();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-
-    }
-
-    public void 지하철_노선_수정(Long modifyStationIdx, ModifyLineRequest modifyLineRequest) {
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .body(modifyLineRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put("/lines/" + modifyStationIdx)
-                .then().log().all()
-                .extract();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-
-    }
-
-    public Response 지하철_노선_조회_Return_Response(Long stationIndex) {
-        return RestAssured.given().log().all()
-                .when().get("/lines/" + stationIndex)
-                .then().log().all()
-                .extract().response();
-
-    }
-
-    public CreateLineResponse 지하철_노선_조회(Long stationIndex) {
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when().get("/lines/" + stationIndex)
-                .then().log().all()
-                .extract();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        return response.body().as(CreateLineResponse.class);
-    }
-
-    public RetrieveLineResponse 지하철_전체_노선_조회() {
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when().get("/lines")
-                .then().log().all()
-                .extract();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        return response.body().as(RetrieveLineResponse.class);
-    }
-
-    public CreateLineResponse 지하철_노선_생성(String name, String color, Long upStationId, Long downStationId, Long distance) {
-        CreateLineRequest createLineRequest = new CreateLineRequest(name, color, upStationId,
-                downStationId, distance);
-
-        ExtractableResponse<Response> createdResponse = RestAssured.given().log().all()
-                .body(createLineRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/line")
-                .then().log().all()
-                .extract();
-
-        assertThat(createdResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        return createdResponse.as(CreateLineResponse.class);
-    }
-
-    public List<CreateStationResponse> 여러_개의_지하철_역_생성(List<String> stationList) {
-        List<CreateStationResponse> createStationResponseList = new ArrayList<>();
-        for (String station : stationList) {
-            CreateStationResponse createStationResponse = 지하철_노선도_등록(station);
-            createStationResponseList.add(createStationResponse);
-        }
-        createStationResponseList.sort(Comparator.comparingLong(CreateStationResponse::getId));
-
-        return createStationResponseList;
-    }
-
-    public CreateStationResponse 지하철_노선도_등록(String stationName) {
-        Map<String, String> createdStationParams = new HashMap<>();
-        createdStationParams.put("name", stationName);
-
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .body(createdStationParams)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all()
-                .extract();
-        Long stationId_response = response.body().jsonPath().getLong("id");
-        String stationName_response = response.body().jsonPath().getString("name");
-
-        return new CreateStationResponse(stationId_response, stationName_response);
-    }
 }
