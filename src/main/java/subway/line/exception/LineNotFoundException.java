5ef5f41d0cac26a3981893660ecb403ab006674b
santos019 <santos019@naver.com>
2024-07-17 23:34:31 +0900

    style: refactor code for style in LineNotFoundException class

--- src/main/java/subway/line/exception/LineNotFoundException.java
+++ src/main/java/subway/line/exception/LineNotFoundException.java
@@ -2,20 +2,8 @@ package subway.line.exception;
 
 public class LineNotFoundException extends RuntimeException{
 
-    public LineNotFoundException() {
-        super();
-    }
-
     public LineNotFoundException(String message) {
         super(message);
-    }
-
-    public LineNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public LineNotFoundException(Throwable cause) {
-        super(cause);
     }
 
     protected LineNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
