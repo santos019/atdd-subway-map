2349080923999ffa314de80d8c09e025b2c9e9d2
santos019 <santos019@naver.com>
2024-07-17 23:32:53 +0900

    style: refactor code for style in LineRepository interface

--- src/main/java/subway/line/repository/LineRepository.java
+++ src/main/java/subway/line/repository/LineRepository.java
@@ -2,7 +2,6 @@ package subway.line.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import subway.line.entity.Line;
 
 import java.util.List;
