b461a55a63c396a1634f1656adc0e7e165346993
santos019 <santos019@naver.com>
2024-07-17 23:38:53 +0900

    test: refactor code for style in LineStep class

--- src/test/java/subway/util/LineStep.java
+++ src/test/java/subway/util/LineStep.java
@@ -6,33 +6,33 @@ import io.restassured.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import subway.line.dto.CreateLineRequest;
-import subway.line.dto.CreateLineResponse;
+import subway.line.dto.LineResponse;
 import subway.line.dto.ModifyLineRequest;
-import subway.line.dto.RetrieveLineResponse;
+import subway.line.dto.LinesResponse;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LineStep {
 
-    public static CreateLineResponse 지하철_노선_조회(Long stationIndex) {
+    public static LineResponse 지하철_노선_조회(Long stationIndex) {
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .when().get("/lines/" + stationIndex)
                 .then().log().all()
                 .extract();
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        return response.body().as(CreateLineResponse.class);
+        return response.body().as(LineResponse.class);
     }
 
-    public static RetrieveLineResponse 지하철_전체_노선_조회() {
+    public static LinesResponse 지하철_전체_노선_조회() {
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .when().get("/lines")
                 .then().log().all()
                 .extract();
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        return response.body().as(RetrieveLineResponse.class);
+        return response.body().as(LinesResponse.class);
     }
 
-    public static CreateLineResponse 지하철_노선_생성(String name, String color, Long upStationId, Long downStationId, Long distance) {
+    public static LineResponse 지하철_노선_생성(String name, String color, Long upStationId, Long downStationId, Long distance) {
         CreateLineRequest createLineRequest = new CreateLineRequest(name, color, upStationId,
                 downStationId, distance);
 
@@ -45,7 +45,7 @@ public class LineStep {
 
         assertThat(createdResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
-        return createdResponse.as(CreateLineResponse.class);
+        return createdResponse.as(LineResponse.class);
     }
 
     public static void 지하철_노선_수정(Long modifyStationIdx, ModifyLineRequest modifyLineRequest) {
